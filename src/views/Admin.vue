192830e2c35966c9f9060c1be954e17bf96e32ae
Luis Ramsden <lsgrrd@gmail.com>
2023-06-03 02:16:48 -0600

    go to room and room participants

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -2,21 +2,26 @@
  <main>
     <div class="container mx-sm mx-auto py-5 px-4">
       <div v-if="rooms.length > 0">
-        <div v-for="room in rooms" class="py-2 pl-5 pr-2 bg-slate-400 dark:bg-slate-800 rounded-full flex justify-between mb-3">
-          <div class="text-xl">
-            {{ room.id }} - 
-            <span class="text-sm">
+        <div v-for="room in rooms" class="py-2 pl-5 pr-2 bg-slate-400 dark:bg-slate-800 rounded-full flex justify-between mb-3 cursor-pointer" @click.self="router.push({ name: 'room', params: { id:room.id }})">
+          <div class="text-xl flex items-center">
+            {{ room.id }}
+            <div class="text-sm ml-2">
+              {{room.offer?.by}} & {{room.answer?.by}}
+            </div>
+          </div>
+          <div class="flex items-center">
+            <span class="text-sm mr-5">
               {{ dayjs(room.createdAt).fromNow() }}
             </span>
+            <div class="rounded-full aspect-square flex items-center align-center hover:bg-red-600 hover:text-white cursor-pointer py-1 px-2"
+              @click="deleteRoom(room.id)"
+            >
+              <span class="material-symbols-outlined text-base">
+                delete
+              </span>
+            </div>
           </div>
 
-          <div class="rounded-full aspect-square flex items-center align-center hover:bg-red-600 hover:text-white cursor-pointer py-1 px-2"
-            @click="deleteRoom(room.id)"
-          >
-            <span class="material-symbols-outlined text-base">
-              delete
-            </span>
-          </div>
 
         </div>
       </div>
@@ -35,8 +40,11 @@ import db from '../firebase/init.js';
 import { collection, query, onSnapshot, deleteDoc, doc } from "firebase/firestore";
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import { useRouter } from 'vue-router';
 dayjs.extend(relativeTime)
 
+
+const router = useRouter();
 const rooms = ref([]);
 
 const deleteRoom = async (id) => {
@@ -50,7 +58,7 @@ onMounted(async () => {
     rooms.value = [];
     querySnapshot.forEach((doc) => {
       console.log(doc.id, " => ", doc.data());
-      rooms.value.push({ id: doc.id, createdAt: doc.data().offer.createdAt });
+      rooms.value.push({ id: doc.id, createdAt: doc.data().offer.createdAt, ...doc.data() });
     });
   });
 
