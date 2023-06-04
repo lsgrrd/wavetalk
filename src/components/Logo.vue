5e5206057518a0c2d2c61e70265132252dc3da82
Luis Ramsden <lsgrrd@gmail.com>
2023-06-04 13:01:57 -0600

    new logo and illustration

---
+++ src/components/Logo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="logo-wrap">
+    <div class="text">WAVE TALK</div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  const text = document.querySelector(".text");
+  text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+  const element = document.querySelectorAll("span");
+  for (let i = 0; i < element.length; i++) {
+    element[i].style.animationDelay = `${i * 0.4}s`;
+  }
+})
+</script>
+
+<style lang="scss" >
+
+.logo-wrap {
+  position: relative;
+
+  .text {
+    position: relative;
+    span {
+      position: relative;
+      display: inline-block;
+      animation: wave 7s ease-in-out infinite;
+    }
+  }
+}
+
+
+@keyframes wave {
+  0% {
+    transform: translateY(0px);
+  }
+  50% {
+    transform: translateY(-20px);
+  }
+  100% {
+    transform: translateY(0px);
+  }
+}
+
+</style>
